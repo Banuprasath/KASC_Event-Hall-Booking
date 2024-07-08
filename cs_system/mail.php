6726d69803d1e2752271e98bebdaf971ba1c4de9
Banu Prasath <92842537+Banuprasath@users.noreply.github.com>
2024-07-08 08:42:12 +0530

    Update mail.php
    security fix

--- cs_system/mail.php
+++ cs_system/mail.php
@@ -18,7 +18,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'banuprasath.dev@gmail.com';                     //SMTP username
-    $mail->Password   = 'tqbv loqi ncyy oipg';                               //SMTP password
+    $mail->Password   = 'add your password';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
